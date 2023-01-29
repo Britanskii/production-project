c7e7dac8f6867cf0877020160cab36b293924d27
britanskii <ohinkovskii@gmail.com>
2023-01-29 18:58:30 +0400

    add config

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,12 @@
+import {BuildOptions} from "./types/config";
+import type {Configuration as DevServerConfiguration} from "webpack-dev-server";
+
+const buildDevServer = ({port}: BuildOptions): DevServerConfiguration => {
+	return {
+		port,
+		open: true,
+		historyApiFallback: true
+	}
+}
+
+export default buildDevServer
