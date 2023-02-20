8e7fcf5c04b5d641ed27847cfa3634e94ae58238
britanskii <ohinkovskii@gmail.com>
2023-02-20 23:45:04 +0400

    Fix: new images

--- src/widgets/sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -2,26 +2,26 @@ import React from "react"
 import {  cleanup, fireEvent } from "@testing-library/react"
 import "@testing-library/jest-dom/extend-expect"
 import { Sidebar } from "./Sidebar"
-import { renderWithTranslation } from "shared/lib/tests/renderWithTranslation"
+import { componentRender } from "shared/lib/tests/componentRender"
 
 afterEach(cleanup)
 
 describe("Sidebar component", () => {
 	it("renders the sidebar component with the correct class names", () => {
-		const { getByTestId } = renderWithTranslation(<Sidebar />)
+		const { getByTestId } = componentRender(<Sidebar />)
 		const sidebar = getByTestId("sidebar")
 		expect(sidebar).toHaveClass("sidebar")
 	})
 
 	it("renders the sidebar component with the correct class names and custom class name", () => {
-		const { getByTestId } = renderWithTranslation(<Sidebar className="custom-class"/>)
+		const { getByTestId } = componentRender(<Sidebar className="custom-class"/>)
 		const sidebar = getByTestId("sidebar")
 		expect(sidebar).toHaveClass("sidebar")
 		expect(sidebar).toHaveClass("custom-class")
 	})
 
 	it("renders the collapsed sidebar component", () => {
-		const { getByTestId } = renderWithTranslation(<Sidebar />)
+		const { getByTestId } = componentRender(<Sidebar />)
 		const sidebar = getByTestId("sidebar")
 		const toggleButton = getByTestId("sidebar-toggle")
 		fireEvent.click(toggleButton)
